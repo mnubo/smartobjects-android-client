def43c2ef8974230d556954f362f3c2f37b2b7da
David Francoeur <dfrancoeur@mnubo.com>
2015-03-09 14:53:05 -0400

    moving demo app to another project

--- app/src/main/java/com/mnubo/platform/android/demo/intentutils/Actions.java
+++
@@ -1,5 +0,0 @@
-package com.mnubo.platform.android.demo.intentutils;
-
-public interface Actions {
-    public static String ACTION_TOAST_MESSAGE = "com.mnubo.intentutils.actions.ACTION_TOAST_MESSAGE";
-}
