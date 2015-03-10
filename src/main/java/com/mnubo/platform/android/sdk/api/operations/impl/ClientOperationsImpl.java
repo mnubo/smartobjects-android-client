26787e5f2df7818cf1ab666fd33ca989c9544051
David Francoeur <dfrancoeur@mnubo.com>
2015-03-10 17:11:21 -0400

    Added the Task object to ClientOperations

--- src/main/java/com/mnubo/platform/android/sdk/api/operations/impl/ClientOperationsImpl.java
+++ src/main/java/com/mnubo/platform/android/sdk/api/operations/impl/ClientOperationsImpl.java
@@ -2,6 +2,7 @@ package com.mnubo.platform.android.sdk.api.operations.impl;
 
 import com.mnubo.platform.android.sdk.api.MnuboApiFactory;
 import com.mnubo.platform.android.sdk.api.operations.ClientOperations;
+import com.mnubo.platform.android.sdk.api.operations.impl.tasks.impl.TaskImpl;
 import com.mnubo.platform.android.sdk.internal.client.api.MnuboClientApi;
 import com.mnubo.platform.android.sdk.internal.user.api.MnuboUserApi;
 import com.mnubo.platform.android.sdk.models.security.ResetPassword;
@@ -14,7 +15,7 @@ import static com.mnubo.platform.android.sdk.api.MnuboApi.CompletionCallBack;
 
 public class ClientOperationsImpl extends AbstractMnuboOperations implements ClientOperations {
 
-    private final static String ACTIVITY_TAG = ClientOperationsImpl.class.getName();
+    private final static String OPERATION_TAG = ClientOperationsImpl.class.getName();
 
 
     public ClientOperationsImpl(MnuboApiFactory.ConnectionOperations connectionOperations,
@@ -25,50 +26,50 @@ public class ClientOperationsImpl extends AbstractMnuboOperations implements Cli
 
     @Override
     public void createUser(final User user, final CompletionCallBack<Boolean> completionCallBack) {
-        executeWithRefresh(new MnuboOperation<Boolean>() {
+        execute(new TaskImpl<>(new MnuboOperation<Boolean>() {
             @Override
             public Boolean executeMnuboCall() {
                 getClientApi().clientSdkOperations().createUser(user);
                 return true;
             }
-        }, completionCallBack);
+        }), completionCallBack);
     }
 
     @Override
     public void confirmUserCreation(final String username, final UserConfirmation confirmation, final CompletionCallBack<Boolean> completionCallBack) {
-        executeWithRefresh(new MnuboOperation<Boolean>() {
+        execute(new TaskImpl<>(new MnuboOperation<Boolean>() {
             @Override
             public Boolean executeMnuboCall() {
                 getClientApi().clientSdkOperations().confirmUserCreation(username, confirmation);
                 return true;
             }
-        }, completionCallBack);
+        }), completionCallBack);
     }
 
     @Override
     public void resetPassword(final String username, final CompletionCallBack<Boolean> completionCallBack) {
-        executeWithRefresh(new MnuboOperation<Boolean>() {
+        execute(new TaskImpl<>(new MnuboOperation<Boolean>() {
             @Override
             public Boolean executeMnuboCall() {
                 getClientApi().clientSdkOperations().resetPassword(username);
                 return true;
             }
-        }, completionCallBack);
+        }), completionCallBack);
     }
 
     @Override
     public void confirmPasswordReset(final String username, final ResetPassword resetPassword, final CompletionCallBack<Boolean> completionCallBack) {
-        executeWithRefresh(new MnuboOperation<Boolean>() {
+        execute(new TaskImpl<>(new MnuboOperation<Boolean>() {
             @Override
             public Boolean executeMnuboCall() {
                 getClientApi().clientSdkOperations().confirmPasswordReset(username, resetPassword);
                 return true;
             }
-        }, completionCallBack);
+        }), completionCallBack);
     }
 
     @Override
     public String getOperationTag() {
-        return ACTIVITY_TAG;
+        return OPERATION_TAG;
     }
 }
