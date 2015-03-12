f441dc1c7638bcfb3c0660a623af2e57af0ef3c3
David Francoeur <dfrancoeur@mnubo.com>
2015-03-12 09:17:55 -0400

    Added  ClientOperationsImpl test

--- src/test/java/com/mnubo/platform/android/sdk/api/operations/impl/ClientOperationsImplTest.java
+++ src/test/java/com/mnubo/platform/android/sdk/api/operations/impl/ClientOperationsImplTest.java
@@ -1,11 +1,38 @@
 package com.mnubo.platform.android.sdk.api.operations.impl;
 
-import junit.framework.TestCase;
+import android.os.AsyncTask;
 
-public class ClientOperationsImplTest extends TestCase {
+import com.mnubo.platform.android.sdk.api.MnuboApi;
+import com.mnubo.platform.android.sdk.api.operations.AbstractOperationsTest;
+import com.mnubo.platform.android.sdk.api.operations.impl.tasks.Task;
+import com.mnubo.platform.android.sdk.models.users.User;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ClientOperationsImplTest extends AbstractOperationsTest {
+
+    private final ClientOperationsImpl clientOperations = new ClientOperationsImpl(mockedConnectionOperations, mockedClientApiConnection, mockedUserApiConnection);
+
+    @SuppressWarnings("unchecked")
+    AsyncTask<Void, Void, Boolean> mockedAsyncTask = mock(AsyncTask.class);
+
+    @SuppressWarnings("unchecked")
+    final MnuboApi.CompletionCallBack<Boolean> callback = mock(MnuboApi.CompletionCallBack.class);
 
     public void testCreateUser() throws Exception {
 
+        when(mockedAsyncTaskFactory.create(any(Task.class), any(MnuboApi.CompletionCallBack.class)))
+                .thenReturn(mockedAsyncTask);
+
+        final User createdUser = new User();
+        createdUser.setUsername("username");
+
+        clientOperations.createUser(createdUser, callback);
+        verify(mockedAsyncTask, only()).execute();
     }
 
     public void testConfirmUserCreation() throws Exception {
