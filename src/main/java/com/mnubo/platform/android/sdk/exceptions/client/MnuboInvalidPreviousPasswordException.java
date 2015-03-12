c424314e3570d3dec55e0aa8e55b796606aa107a
David Francoeur <dfrancoeur@mnubo.com>
2015-03-12 17:10:25 -0400

    Fixed javadoc

--- src/main/java/com/mnubo/platform/android/sdk/exceptions/client/MnuboInvalidPreviousPasswordException.java
+++ src/main/java/com/mnubo/platform/android/sdk/exceptions/client/MnuboInvalidPreviousPasswordException.java
@@ -3,8 +3,8 @@ package com.mnubo.platform.android.sdk.exceptions.client;
 import com.mnubo.platform.android.sdk.Strings;
 
 /**
- * This exception is raised when you perform a request to
- * confirm an user registration but provide the wrong token.
+ * This exception is raised when you try to change an user's password
+ * but the provided previous password is incorrect.
  */
 public class MnuboInvalidPreviousPasswordException extends MnuboClientException {
     public final static String INVALID_PREVIOUS_PASSWORD = "Invalid previous password.";
--- src/main/java/com/mnubo/platform/android/sdk/exceptions/client/MnuboObjectNotFoundException.java
+++ src/main/java/com/mnubo/platform/android/sdk/exceptions/client/MnuboObjectNotFoundException.java
@@ -3,8 +3,8 @@ package com.mnubo.platform.android.sdk.exceptions.client;
 import com.mnubo.platform.android.sdk.Strings;
 
 /**
- * This exception is raised when you perform a request to
- * confirm an user registration but provide the wrong token.
+ * This exception is raised when you try to fetch an object that doesn't
+ * exists in the mnubo API.
  */
 public class MnuboObjectNotFoundException extends MnuboClientException {
     public final static String OBJECT_NOT_FOUND = "Object not found";
