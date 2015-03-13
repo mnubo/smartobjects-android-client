4e57e711f732b3c70f1d0a47523b86d57499b57e
David Francoeur <dfrancoeur@mnubo.com>
2015-03-13 14:31:24 -0400

    Clean mnubo api

--- src/test/java/com/mnubo/platform/android/sdk/MnuboTest.java
+++ src/test/java/com/mnubo/platform/android/sdk/MnuboTest.java
@@ -3,15 +3,12 @@ package com.mnubo.platform.android.sdk;
 import android.content.Context;
 import android.util.Log;
 
-import com.mnubo.platform.android.sdk.api.MnuboApi;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static junit.framework.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
@@ -23,20 +20,58 @@ import static org.powermock.api.mockito.PowerMockito.mockStatic;
 public class MnuboTest {
 
     private final Context mockedContext = mock(Context.class);
-    private final MnuboApi mockedApi = mock(MnuboApi.class);
 
     @Before
     public void setUp() throws Exception {
         mockStatic(Log.class);
     }
 
-    @Test
-    public void testGetApi() throws Exception {
-        fail();
+    @Test(expected = NullPointerException.class)
+    public void testInitNoContext() throws Exception {
+
+        Mnubo.init(null, "key", "secret", "hostname");
+
     }
 
-    @Test
-    public void testGetUsername() throws Exception {
+    @Test(expected = NullPointerException.class)
+    public void testInitNullKey() throws Exception {
+
+        Mnubo.init(mockedContext, null, "secret", "hostname");
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInitEmptyKey() throws Exception {
+
+        Mnubo.init(mockedContext, "", "secret", "hostname");
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testInitNullSecret() throws Exception {
+
+        Mnubo.init(mockedContext, "key", null, "hostname");
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInitEmptySecret() throws Exception {
+
+        Mnubo.init(mockedContext, "key", "", "hostname");
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testInitNullHostname() throws Exception {
+
+        Mnubo.init(mockedContext, "key", "secret", null);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInitEmptyHostname() throws Exception {
+
+        Mnubo.init(mockedContext, "key", "secret", "");
 
     }
 }
