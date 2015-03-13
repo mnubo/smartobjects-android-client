e2cd5dd379629366a8c5e4eb5b704e662aa68b4b
David Francoeur <dfrancoeur@mnubo.com>
2015-03-13 13:41:52 -0400

    Add some test, but I realized maybe I could remove a layer of complexity

--- src/test/java/com/mnubo/platform/android/sdk/MnuboTest.java
+++ src/test/java/com/mnubo/platform/android/sdk/MnuboTest.java
@@ -4,18 +4,21 @@ import android.content.Context;
 import android.util.Log;
 
 import com.mnubo.platform.android.sdk.api.MnuboApi;
+import com.mnubo.platform.android.sdk.api.MnuboApiFactory;
+import com.mnubo.platform.android.sdk.exceptions.sdk.MnuboNotInitializedException;
 
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
@@ -23,82 +26,30 @@ import static org.powermock.api.mockito.PowerMockito.whenNew;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({
         Log.class,
-        PlainSocketFactory.class,
-        SSLSocketFactory.class,
-        HttpComponentsClientHttpRequestFactory.class
+        Mnubo.class
 })
 public class MnuboTest {
 
     private final Context mockedContext = mock(Context.class);
-    private final PlainSocketFactory mockedPlainSocketFactory = mock(PlainSocketFactory.class);
-    private final SSLSocketFactory mockedSSLSocketFactory = mock(SSLSocketFactory.class);
-    private final SchemeRegistry mockedSchemeRegistry = mock(SchemeRegistry.class);
+    private final MnuboApiFactory mockedApiFactory = mock(MnuboApiFactory.class);
+    private final MnuboApi mockedApi = mock(MnuboApi.class);
 
     @Before
     public void setUp() throws Exception {
         mockStatic(Log.class);
-        mockStatic(PlainSocketFactory.class);
-        mockStatic(SchemeRegistry.class);
-        mockStatic(SSLSocketFactory.class);
+        whenNew(MnuboApiFactory.class).withAnyArguments().thenReturn(mockedApiFactory);
 
-        whenNew(SchemeRegistry.class).withAnyArguments().thenReturn(mockedSchemeRegistry);
-        when(PlainSocketFactory.getSocketFactory()).thenReturn(mockedPlainSocketFactory);
-        when(SSLSocketFactory.getSocketFactory()).thenReturn(mockedSSLSocketFactory);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testInitNoContext() throws Exception {
-
-        Mnubo.init(null, "key", "secret", "hostname");
-
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testInitNullKey() throws Exception {
-
-        Mnubo.init(mockedContext, null, "secret", "hostname");
-
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testInitEmptyKey() throws Exception {
-
-        Mnubo.init(mockedContext, "", "secret", "hostname");
-
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testInitNullSecret() throws Exception {
-
-        Mnubo.init(mockedContext, "key", null, "hostname");
-
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testInitEmptySecret() throws Exception {
-
-        Mnubo.init(mockedContext, "key", "", "hostname");
-
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testInitNullHostname() throws Exception {
-
-        Mnubo.init(mockedContext, "key", "secret", null);
-
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testInitEmptyHostname() throws Exception {
-
-        Mnubo.init(mockedContext, "key", "secret", "");
-
+        when(mockedApiFactory.getApi()).thenReturn(mockedApi);
     }
 
     @Test
     public void testGetApi() throws Exception {
         Mnubo.init(mockedContext, "key", "secret", "hostname");
+
         MnuboApi api = Mnubo.getApi();
+        assertThat(api, is(equalTo(mockedApi)));
+
+        verify(mockedApiFactory, only()).getApi();
     }
 
     @Test
