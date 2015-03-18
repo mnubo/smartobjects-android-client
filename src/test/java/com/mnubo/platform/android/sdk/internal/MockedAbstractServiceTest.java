666efbefa2c7093786978e786d8b1e03a721ebaa
David Francoeur <dfrancoeur@mnubo.com>
2015-03-18 11:34:47 -0400

    Add MIT license to all files

--- src/test/java/com/mnubo/platform/android/sdk/internal/MockedAbstractServiceTest.java
+++ src/test/java/com/mnubo/platform/android/sdk/internal/MockedAbstractServiceTest.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2015 Mnubo. Released under MIT License.
+ *
+ *     Permission is hereby granted, free of charge, to any person obtaining a copy
+ *     of this software and associated documentation files (the "Software"), to deal
+ *     in the Software without restriction, including without limitation the rights
+ *     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *     copies of the Software, and to permit persons to whom the Software is
+ *     furnished to do so, subject to the following conditions:
+ *
+ *     The above copyright notice and this permission notice shall be included in
+ *     all copies or substantial portions of the Software.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *     THE SOFTWARE.
+ */
 package com.mnubo.platform.android.sdk.internal;
 
 import com.mnubo.platform.android.sdk.models.collections.Collection;
@@ -20,7 +41,6 @@ import static org.mockito.Mockito.mock;
 public abstract class MockedAbstractServiceTest {
 
     protected static final String PLATFORM_BASE_URL = "https://sandbox.mnubo.com";
-    protected static final String USER_ACCESS_TOKEN = "ACCESS_TOKEN";
 
     protected final Group expectedGroup = new Group();
     protected final Collection expectedCollection = new Collection();
