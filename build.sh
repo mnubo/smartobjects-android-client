35829da2be60bf3f2268af1eda5f65f089edd292
David Francoeur <dfrancoeur04@gmail.com>
2016-10-20 18:11:12 -0400

    fix(build): badly encoded secring

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo 'RELEASE'
     # encrypted using `travis encrypt-file`
-    # encrypted variables can only be used for build on upstream, not forls
+    # encrypted variables can only be used for build on upstream, not forks
     openssl aes-256-cbc -K $encrypted_8bb023dc1fa3_key -iv $encrypted_8bb023dc1fa3_iv -in gradle/keys.xml.enc -out sdk-android-test/src/main/res/values/keys.xml -d
     openssl aes-256-cbc -K $encrypted_8bb023dc1fa3_key -iv $encrypted_8bb023dc1fa3_iv -in gradle/mnubo.secring.gpg.enc -out gradle/mnubo.secring.gpg -d
     ./gradlew -S clean sdk-android-lib:build sdk-android-test:connectedAndroidTest sdk-android-lib:uploadArchives sdk-android-lib:closeRepository sdk-android-lib:promoteRepository
