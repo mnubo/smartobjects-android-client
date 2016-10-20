d56a4976ca357e23f09f07917c7e35428c21110f
David Francoeur <dfrancoeur04@gmail.com>
2016-10-20 18:41:14 -0400

    debug(travis): trying to debug encrypted values

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo 'RELEASE'
     # encrypted using `travis encrypt-file`
-    # encrypted variables can only be used for build on upstream, not forks
-    openssl aes-256-cbc -K $encrypted_8bb023dc1fa3_key -iv $encrypted_8bb023dc1fa3_iv -in gradle/keys.xml.enc -out sdk-android-test/src/main/res/values/keys.xml -d
-    openssl aes-256-cbc -K $encrypted_8bb023dc1fa3_key -iv $encrypted_8bb023dc1fa3_iv -in gradle/mnubo.secring.gpg.enc -out gradle/mnubo.secring.gpg -d
+    openssl aes-256-cbc -K $encrypted_8bb023dc1fa3_key -iv $encrypted_8bb023dc1fa3_iv -in gradle/data.tar.enc -out gradle/data.tar -d
+
+    tar -xvf gradle/data.tar -C gradle/
+    mv gradle/keys.xml sdk-android-test/src/main/res/values/
+
     ./gradlew -S clean sdk-android-lib:build sdk-android-test:connectedAndroidTest sdk-android-lib:uploadArchives sdk-android-lib:closeRepository sdk-android-lib:promoteRepository
 else
     echo 'BUILDING MR'
-    echo 'INTEGRATION TESTS ARE NOT RAN'
-    ./gradlew -S clean sdk-android-lib:build
+    ./gradlew -S clean sdk-android-lib:build sdk-android-test:connectedAndroidTest
 fi
---
+++ gradle/info.md
@@ -0,0 +1,4 @@
+Two files in this tar:
+
+- keys.xml which contains consumer key/secret to run integration tests. this file has to be copied in: sdk-android-test/src/main/res/values/
+- mnubo.secring.gpg which is used to sign the artifacts
