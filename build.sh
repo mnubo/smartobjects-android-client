1f73db7df4c96bc864435ab4596107394c5d1a71
David Francoeur <dfrancoeur04@gmail.com>
2016-10-20 12:51:47 -0400

    fix(build): provide key/secret via encrypted file for travis

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# override key/secret with what is available from ENV variables
-sed -i "" -e "s/KEY_VALUE/$CONSUMER_KEY/g" sdk-android-test/src/main/res/values/keys.xml
-sed -i "" -e "s/SECRET_VALUE/$CONSUMER_SECRET/g" sdk-android-test/src/main/res/values/keys.xml
-
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo 'RELEASE'
     # encrypted using `travis encrypt-file`
+    # encrypted variables can only be used for build on upstream, not forls
+    openssl aes-256-cbc -K $encrypted_8bb023dc1fa3_key -iv $encrypted_8bb023dc1fa3_iv -in gradle/keys.xml.enc -out sdk-android-test/src/main/res/values/keys.xml -d
     openssl aes-256-cbc -K $encrypted_8bb023dc1fa3_key -iv $encrypted_8bb023dc1fa3_iv -in gradle/mnubo.secring.gpg.enc -out gradle/mnubo.secring.gpg -d
     ./gradlew -S clean sdk-android-lib:build sdk-android-test:connectedAndroidTest sdk-android-lib:uploadArchives sdk-android-lib:closeRepository sdk-android-lib:promoteRepository
 else
     echo 'BUILDING MR'
-    ./gradlew -S clean sdk-android-lib:build sdk-android-test:connectedAndroidTest
+    echo 'INTEGRATION TESTS ARE NOT RAN'
+    ./gradlew -S clean sdk-android-lib:build
 fi
